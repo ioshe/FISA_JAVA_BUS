8838a3eee51131a46b84f5e4ff196362e9bf829f
Ryunos96 <sinple010@gmail.com>
2024-01-12 15:39:34 +0900

    data_time

--- BusReservation/ReserveEx.java
+++ BusReservation/ReserveEx.java
@@ -4,6 +4,14 @@ public class ReserveEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// 시간대별 버스 가격
+		int[] intercityPricesAM = {5000, 15000, 25000, 35000}; // AM - 6:00
+		int[] intercityPricesPM = {10000, 20000, 30000, 40000}; // PM - 12:00, 18:00
+		int[] intercityPricesNight = {7500, 17500, 27500, 37500}; //Night - 24:00
+		int[] expressPricesAM = {10000, 20000, 30000, 40000};
+		int[] expressPricesPM = {15000, 25000, 35000, 45000};
+		int[] expressPricesNight = {12500, 22500, 32500, 42500};
 		Input cus1= new Input();
 		cus1.create();
 	}
