609215c07d0d10809cf168857084fdb3767389e5
ioshe <sdcard11291@gmail.com>
2024-01-12 15:52:10 +0900

    bus_init_2

--- BusReservation/Bus.java
+++ BusReservation/Bus.java
@@ -1,5 +1,43 @@
 package BusReservation;
 
-public class Bus {
+import java.util.ArrayList;
 
+public class Bus {
+	static ArrayList<Bus> BusInfo = new ArrayList<>(); 
+	String time;
+	String startLoc;
+	String endLoc;
+	int[] seat = new int[20];
+	int[] prices = new int[4];
+	String busName;
+	String busType;
+	
+	Bus(String busName,String startLoc,String endLoc,String time,String type,int[] prices) {
+		this.busName = busName;
+	    this.startLoc = startLoc;
+	    this.endLoc = endLoc;
+	    this.time = time;
+	    this.busType = busType;
+	    
+	    BusInfo.add(this);
+	}
+	
+	static ArrayList busGetNames(){
+		ArrayList<String> busNames = new ArrayList<>();
+		for (Bus bus : BusInfo) {
+			busNames.add(bus.busName);
+		}
+		return busNames;
+	}
+	
+	static ArrayList<Bus> findLocation(String startLoc,String endLoc) {
+		ArrayList<Bus> correctbus = new ArrayList<>();
+		for (Bus bus : BusInfo) {
+			System.out.println(bus.busName);
+			if (bus.startLoc.equals(startLoc) && (bus.endLoc.equals(endLoc))) {
+				correctbus.add(bus);
+			}
+		}
+		return correctbus; 
+	}
 }
